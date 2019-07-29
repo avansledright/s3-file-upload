f19ccc7ad4fb8672a7fd8a976ddcc045d37fb827
Aaron VanSledright <avansledright@gmail.com>
2019-07-29 14:48:57 -0400

    Working with test dockerfile

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import boto3
+import os
 
 
-s3 = boto3.client('s3')
-filename = 'file.txt'
-bucket_name = 'vansledright-personal-backup'
+s3_resource = boto3.resource(
+    's3',
+    aws_access_key_id='ID',
+    aws_secret_access_key='KEY'
+)
+def upload_objects():
+    try:
+        root_path = '/ROOTPATHHERE'
+        bucket_name = 'BUCKETNAMEHERE'
+        my_bucket = s3_resource.Bucket(bucket_name)
 
-s3.upload_file(filename, bucket_name, filename)
+        for path, subdirs, files in os.walk(root_path) :
+            path = path.replace("\\","/")
+            directory_name = path.replace(root_path,"")
+            for file in files:
+                my_bucket.upload_file(os.path.join(path, file), directory_name+'/'+file)
+
+    except Exception as err:
+        print(err)
+
+if __name__ == '__main__':
+    upload_objects()
