4bcf026fed5f1b11d4330093ae397d847093e263
Aaron VanSledright <avansledright@gmail.com>
2019-07-31 14:54:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import boto3
 import os
-
+import sys
 
 s3_resource = boto3.resource(
     's3',
-    aws_access_key_id='ID',
-    aws_secret_access_key='KEY'
+    aws_access_key_id=sys.argv[1],
+    aws_secret_access_key=sys.argv[2]
 )
 def upload_objects():
     try:
-        root_path = '/ROOTPATHHERE'
-        bucket_name = 'BUCKETNAMEHERE'
+        root_path = sys.argv[3]
+        bucket_name = sys.argv[4]
         my_bucket = s3_resource.Bucket(bucket_name)
 
         for path, subdirs, files in os.walk(root_path) :
-            path = path.replace("\\","/")
+            path = path.replace("\\", "/")
             directory_name = path.replace(root_path,"")
             for file in files:
                 my_bucket.upload_file(os.path.join(path, file), directory_name+'/'+file)
